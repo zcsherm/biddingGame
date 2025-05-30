7e4cbceed7b98f551ed6b312ca008416b90c3eb7
zcsherm <165740629+zcsherm@users.noreply.github.com>
2025-05-30 11:19:49 -0700

    Update collections.py

--- collections.py
+++ collections.py
@@ -31,6 +31,9 @@ class Collection:
         
     def get_items(self):
         return self._items
+
+    def get_item(self, index):
+        return self._items[index]
     
 class Cart(collection):
     def __init__(self, owner):
@@ -41,7 +44,8 @@ class Cart(collection):
 
     def discard_item(self, item):
         super().remove_item(item)
-        self._owner(None)
+        item.set_owner(None)
+        # Move to market
         
 class Bundle(collection):
     
@@ -55,3 +59,56 @@ class Bundle(collection):
     def remove_item(self, item):
         super().remove_item(item)
         self._owner.get_cart().add_item(item)
+
+    def discard_item(self, item):
+        super().remove_item(item)
+        item.set_owner(None)
+
+class Market(Collection):
+
+    def __init__(self, count):
+        super().__init__('game')
+        self._capacity = count
+        self._slots = {i:None for i in range(count)}
+        self._used_slots = len(self._items)
+        self.update_item_slots(self._items)
+
+    def update_item_slots(self, items):
+        self._slots = {i:None for i in range(15)}
+        counter = 0
+        for item in self._items:
+            self._slots[counter] = item
+            counter += 1
+        
+        
+    def remove_item(self, item):
+        super().remove_item(item)
+        self._used_slots -= 1
+        for key in self._items:
+            if self._items[key] == item:
+                self._items[key] = None
+                return
+                
+    def add_item(self, item):
+        super().add_item(item)
+        self._used_slots += 1
+        for key in self._items:
+            if self._items[key] == None:
+                self._items[key] = item
+                return
+
+    def refill_slots(self, deck):
+        while self._used_slots < count:
+            self.add_item(deck.draw_card())
+            self._used_slots += 1
+
+class Contracts(Market):
+
+    def __init__(self, count):
+        super().__init__('game')
+        
+    def discard_all(self):
+        for key in self._slots:
+            if self._slots[key] is not None:
+                self.remove_item(self._slots[key])
+        

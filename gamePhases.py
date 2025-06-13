87e3d463ba956295f2941d455461d7497e26354c
zcsherm <165740629+zcsherm@users.noreply.github.com>
2025-06-13 15:19:38 -0700

    Create gamePhases.py

---
+++ gamePhases.py
@@ -0,0 +1,18 @@
+class GamePhase:
+    def __init__(self):
+        pass
+
+class Setup(GamePhase):
+    pass
+
+class Bidding(GamePhase):
+    pass
+
+class Buying(GamePhase):
+    pass
+
+class Scoring(GamePhase):
+    pass
+
+class CleanUp(GamePhase):
+    pass
